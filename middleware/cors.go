3a16fcc9c0f3789ed3b1e69eb87d0ad5528714ff
XiaoBao <lensoso@qq.com>
2025-01-31 21:08:11 +0800

    优化模板代码

--- middleware/cors.go
+++ middleware/cors.go
@@ -12,7 +12,7 @@ func Cors() gin.HandlerFunc {
 		origin := c.Request.Header.Get("Origin")
 
 		if origin != "" {
-			c.Header("Access-Control-Allow-Origin", origin)
+			c.Header("Access-Control-Allow-Origin", "*")
 			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,Token")
 			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
